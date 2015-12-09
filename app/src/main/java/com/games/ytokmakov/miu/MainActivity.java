572ce435f50f54fab6bd915536db11c0d37fed0e
Yegor <voute@yandex.ru>
2015-12-09 14:40:35 +0400

    work commit Gson added

--- app/src/main/java/com/games/ytokmakov/miu/MainActivity.java
+++ app/src/main/java/com/games/ytokmakov/miu/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends Activity {
 
     GameView gameView;
@@ -11,6 +14,18 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        try {
+
+            FileOutputStream output = openFileOutput("my_gile_1", MODE_PRIVATE);
+
+            String text1 = "awegraergaerg aergf a wegr ae g";
+
+            output.write(text1.getBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         gameView = new GameView(this);
 
         super.onCreate(savedInstanceState);
